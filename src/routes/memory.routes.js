532c550300f13e24493fe98e9951ebc2958fe0e2
francisgraphic <francisnyong22@gmail.com>
2026-08-02 00:57:28 +0100

    Fix memory route middleware path

--- src/routes/memory.routes.js
+++ src/routes/memory.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const auth = require("../middlewares/auth.middleware");
+const auth = require("../middleware/auth.middleware");
 
 const memory = require("../controllers/memory.controller");
 
